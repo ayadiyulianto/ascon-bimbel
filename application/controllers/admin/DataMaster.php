d03cfeca8b34c6e40e733d9bf3d0f6b34abc3162
RickySadewa <rickysadewa007@gmail.com>
2019-12-04 16:29:47 +0700

    4/12/19 16:29 up

---
+++ application/controllers/admin/DataMaster.php
@@ -0,0 +1,42 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class DataMaster extends CI_Controller {
+
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/welcome
+	 *	- or -
+	 * 		http://example.com/index.php/welcome/index
+	 *	- or -
+	 * Since this controller is set as the default controller in
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 * @see https://codeigniter.com/user_guide/general/urls.html
+	 */
+	public function pengajar()
+	{
+		$this->load->view('admin/header');
+		$this->load->view('admin/view_pengajar');
+		$this->load->view('admin/footer');
+	}
+
+	public function kelas()
+		{
+			$this->load->view('admin/header');
+			$this->load->view('admin/view_semuakelas');
+			$this->load->view('admin/footer');
+		}
+	
+
+	public function siswa()
+	{
+		$this->load->view('admin/header');
+		$this->load->view('admin/view_siswa');
+		$this->load->view('admin/footer');
+	}
+}
